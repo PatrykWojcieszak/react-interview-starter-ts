5a09dafc69ae432aaa82fa6b98c46b05359b9cf4
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-02-12 11:51:06 +0100

    feat: added redux

--- src/app/products/Products.tsx
+++ src/app/products/Products.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { AppRoute } from "routing/AppRoute.enum";
@@ -7,8 +7,20 @@ import { Header } from "app/shared/header/Header";
 import styled from "styled-components";
 import { NoProducts } from "./productList/noProducts/NoProducts";
 import { device } from "styles/breakpoints";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "store/rootReducer";
+import { fetchProduct } from "store/product/ProductSlice";
 
 export const Products = () => {
+  const { product, loading } = useSelector((root: RootState) => root.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProduct("?limit=8&page=1&promo=false&active=false"));
+  }, []);
+
+  console.log(product, loading);
+
   return (
     <StyledProducts>
       {/* <h2>Products page</h2>
