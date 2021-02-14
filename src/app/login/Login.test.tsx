f13131e35f33b66264b88aea2816aaa0d79bfc39
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-02-14 12:53:26 +0100

    refactor: login test cleanup

--- src/app/login/Login.test.tsx
+++ src/app/login/Login.test.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 
-import { getByAltText, getByRole, render, screen } from "tests";
+import { render } from "tests";
 
 import Login from "./Login";
 
 describe("Login", () => {
   test("Displays all information", async () => {
-    const { getByText, getByLabelText } = render(<Login />);
+    const {
+      getByText,
+      getByLabelText,
+      getByAltText,
+      getByPlaceholderText,
+      getByRole,
+    } = render(<Login />);
 
-    expect(screen.getByAltText("background")).toBeInTheDocument();
+    expect(getByAltText("background")).toBeInTheDocument();
     expect(getByText("join.tsh.io")).toBeInTheDocument();
     expect(getByText("Login")).toBeInTheDocument();
     expect(getByLabelText("Username")).toBeInTheDocument();
     expect(getByLabelText("Password")).toBeInTheDocument();
-    expect(screen.getByPlaceholderText("Enter username")).toBeInTheDocument();
-    expect(screen.getByPlaceholderText("Enter password")).toBeInTheDocument();
-    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(getByPlaceholderText("Enter username")).toBeInTheDocument();
+    expect(getByPlaceholderText("Enter password")).toBeInTheDocument();
+    expect(getByRole("button")).toBeInTheDocument();
     expect(getByText("Forgot password?")).toBeInTheDocument();
   });
 });
