30a33bd7a6d381e929e619ef3ee87a83c2fd5507
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-02-24 22:09:05 +0100

    feat: added api url to env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = "https://join-tsh-api-staging.herokuapp.com/";
 export const PRODUCTS = "products";
 
 export async function getData<T>(endpointURL: string, params?: string) {
-  const { data } = await axios.get<T>(BASE_URL + endpointURL + params);
+  const { data } = await axios.get<T>(
+    process.env.REACT_APP_API_URL + endpointURL + params
+  );
 
   return data;
 }
